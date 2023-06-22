bff8d8eedba6bf67b032bf7c1dbaae23e259f3c7
Rohan Sapate <rhnsapate16@gmail.com>
2023-06-22 10:13:29 +0200

    css-to-the-table

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -48,29 +48,29 @@ export default function Dashboard() {
             <TextField label="Search" variant="outlined" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} InputLabelProps={{ style: { fontSize: 20 } }} inputProps={{ style: { fontSize: 20 } }} />
           </Box>
 
-          <TableContainer component={Paper} style={{ maxWidth: '80%', margin: 'auto', backgroundColor: '#f5f5f5' }}>
+          <TableContainer component={Paper} style={{ maxWidth: '90%', margin: 'auto', backgroundColor: '#B0C4DE' }}>
   <Table style={{ borderCollapse: 'separate', borderSpacing: '0 15px' }}>
     <TableHead>
       <TableRow>
-        <TableCell align="left" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Name</TableCell>
-        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Image</TableCell>
-        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Category</TableCell>
-        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Quantity</TableCell>
-        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Price</TableCell>
-        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #9e9e9e' }}>Total Price</TableCell>
+        <TableCell align="left" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Name</TableCell>
+        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Image</TableCell>
+        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Category</TableCell>
+        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Quantity</TableCell>
+        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Price</TableCell>
+        <TableCell align="center" style={{ fontSize: '20px', fontWeight: 'bold', color: '#3f51b5', borderBottom: '2px solid #191970' }}>Total Price</TableCell>
       </TableRow>
     </TableHead>
     <TableBody>
       {stockInfo.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase())).map((item) => (
         <TableRow key={item.id} style={{ backgroundColor: '#fafafa', borderRadius: '5px', margin: '10px 0' }}>
-          <TableCell component="th" scope="row" style={{ fontSize: '18px', color: '#616161' }}>
+          <TableCell component="th" scope="row" style={{ fontSize: '18px', color: '#8E4585' }}>
             {item.name}
           </TableCell>
           <TableCell align="center"><img src={item.imageUrl} alt="Product" style={{width: "75px", height: "100px"}} /></TableCell>
-          <TableCell align="center" style={{ fontSize: '18px', color: '#616161' }}>{item.category}</TableCell>
-          <TableCell align="center" style={{ fontSize: '18px', color: '#616161' }}>{item.quantity}</TableCell>
-          <TableCell align="center" style={{ fontSize: '18px', color: '#616161' }}>{item.price}</TableCell>
-          <TableCell align="center" style={{ fontSize: '18px', color: '#616161' }}>{item.totalPrice}</TableCell>
+          <TableCell align="center" style={{ fontSize: '18px', color: '#008080' }}>{item.category}</TableCell>
+          <TableCell align="center" style={{ fontSize: '18px', color: '#6B8E23' }}>{item.quantity}</TableCell>
+          <TableCell align="center" style={{ fontSize: '18px', color: '#8FBC8F' }}>{item.price}</TableCell>
+          <TableCell align="center" style={{ fontSize: '18px', color: '#2F4F4F' }}>{item.totalPrice}</TableCell>
         </TableRow>
       ))}
     </TableBody>
