d2dce2ec22f9a15473a2bf3be17be503e4555a7a
Ali Alshareefi <alialshareefi@hotmail.com>
2023-07-04 10:21:17 +0200

    last

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,11 @@ import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
-// import Divider from '@mui/material/Divider';
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
-// import PersonAdd from '@mui/icons-material/PersonAdd';
-// import Settings from '@mui/icons-material/Settings';
 import Logout from "@mui/icons-material/Logout";
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function Navbar({ user, setUser }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -43,92 +40,131 @@ export default function Navbar({ user, setUser }) {
       setTheme("light");
     }
   };
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
 
   return (
-    <div>
-      <Box className={`navbar ${theme}`} sx={{ boxShadow: 3 }}>
-        <div className="logoContainer">
-          <img src={logo} alt="logo" className="logo" />
-          <h1 className="navbarFredoka">Stock Tracker</h1>
-        </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <div>
+        <Box
+          className="navbar"
+          sx={{
+            boxShadow: 3,
+            bgcolor:
+              theme === "light" ? "background.paper" : "background.default",
+            color: theme === "light" ? "text.primary" : "text.secondary",
+          }}
+        >
+          <div className="logoContainer">
+            <img src={logo} alt="logo" className="logo" />
+            <h1 className="navbarFredoka">Stock Tracker</h1>
+          </div>
 
-        <div className="refresdark">
-          <button className="but" onClick={refreshPage}>
-            <RefreshIcon sx={{ color: "#4b5563", marginRight: "0.5rem" }} />{" "}
-            Refresh
-          </button>
+          <div className="refresdark">
+            <button className="but" onClick={refreshPage}>
+              <RefreshIcon sx={{ color: "#4b5563", marginRight: "0.5rem" }} />{" "}
+              Refresh
+            </button>
 
-          <button className="but" onClick={toggleTheme}>
-            <DarkModeIcon sx={{ color: "#4b5563", marginRight: "0.5rem" }} />
-            Dark Mode
-          </button>
-          <Box
-            sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
-          >
-            <Tooltip title="Account settings">
-              <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? "account-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
+            <button
+              className="but"
+              onClick={toggleTheme}
+              sx={{
+                color: theme === "light" ? "#4b5563" : "primary.main",
+                marginRight: "0.5rem",
+              }}
+            >
+              <DarkModeIcon sx={{ color: "#4b5563", marginRight: "0.5rem" }} />
+              Dark Mode
+            </button>
+
+            <Box
+              sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
+            >
+              <Tooltip
+                title="Account settings"
+                sx={{
+                  color: theme === "light" ? "text.secondary" : "primary.main",
+                }}
               >
-                <Avatar sx={{ width: 40, height: 40 }}>
-                  {user.username.charAt(0).toUpperCase()}
-                </Avatar>
-              </IconButton>
-            </Tooltip>
-          </Box>
-          <Menu
-            anchorEl={anchorEl}
-            id="account-menu"
-            open={open}
-            onClose={handleClose}
-            onClick={handleClose}
-            PaperProps={{
-              elevation: 0,
-              sx: {
-                padding: "10px",
-                overflow: "visible",
-                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                mt: 1.5,
-                "& .MuiAvatar-root": {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                "&:before": {
-                  content: '""',
-                  display: "block",
-                  position: "absolute",
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: "background.paper",
-                  transform: "translateY(-50%) rotate(45deg)",
-                  zIndex: 0,
-                },
-              },
-            }}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          >
-            <MenuItem onClick={logout}>
-              <ListItemIcon>
-                <Logout fontSize="small" />
-              </ListItemIcon>
-              Logout
-            </MenuItem>
-          </Menu>
-        </div>
-      </Box>
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  sx={{ ml: 2 }}
+                  aria-controls={open ? "account-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                >
+                  <Avatar sx={{ width: 40, height: 40 }}>
+                    {user.username.charAt(0).toUpperCase()}
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+            </Box>
 
-    </div>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  padding: "10px",
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor:
+                      theme === "light"
+                        ? "background.paper"
+                        : "background.default",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem onClick={logout}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
+          </div>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
